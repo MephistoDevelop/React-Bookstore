4db5d96f6a37cc995052b99c6d1344efb72f63f0
MephistoDevelop <inesherald@gmail.com>
2020-03-06 10:05:04 -0600

    Refactor: iniialize redux store onentire aplication

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './assets/styles/styles.css';
 
-const targetNode = document.getElementById('root');
-ReactDOM.render(<App />, targetNode);
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './components/Book';
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
