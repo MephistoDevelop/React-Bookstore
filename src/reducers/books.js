370fea709de2b8da6808625fb56cb6cb7a437496
MephistoDevelop <inesherald@gmail.com>
2020-03-08 09:05:23 -0600

    eslint fix

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -4,7 +4,6 @@ const books = (state, action) => {
   const newState = { ...state };
   switch (action.type) {
     case 'CREATE_BOOK':
-      console.log('Soy create book acion');
       break;
     case 'DELETE_BOOK':
       break;
