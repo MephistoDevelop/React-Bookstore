e45d4aa7bcc0438f0030a929a4429a2a483adecf
MephistoDevelop <inesherald@gmail.com>
2020-03-10 17:47:45 -0600

    Get category select

--- src/containers/CategoryFilter.js
+++ src/containers/CategoryFilter.js
@@ -3,33 +3,42 @@ import React from 'react';
 import { connect } from 'react-redux';
 import propTypes from 'prop-types';
 
-const CategoryFilter = (props) => {
-  const { categories } = props;
+class CategoryFilter extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const options = () => {
-    console.log(`Categories: ${JSON.stringify(props)}`);
+    this.options = this.options.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+
+  options() {
+    const { categories } = this.props;
+    console.log(`Categories: ${JSON.stringify(this.props)}`);
     return (categories.map((cat) => (
       <option key={cat} name={cat}>
         {' '}
         {cat}
       </option>
     )));
-  };
+  }
 
-  const handleChange = (event) => {
-    const cbx = this;
-    // const categoryName = cbx.options[cbx.selectedIndex].text;
-    console.log(cbx);
-  };
+  handleChange(event) {
+    const cbx = event.target;
+    const categoryName = cbx.options[cbx.selectedIndex].text;
+    console.log(`Soy cbx: ${categoryName}`);
+  }
 
-  return (
-    <div>
-      <select id="cbx-category" onChange={handleChange()}>
-        {options()}
-      </select>
-    </div>
-  );
-};
+  render() {
+    return (
+      <div>
+        <select id="cbx-category" onChange={((e) => this.handleChange(e))}>
+          {this.options()}
+        </select>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = (state) => ({
   books: state.books,
