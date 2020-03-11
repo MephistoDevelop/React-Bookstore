f31b42e39416795a2be291e07a905533f60332ec
MephistoDevelop <inesherald@gmail.com>
2020-03-11 11:00:57 -0600

    Add progressbar and UI styles

--- src/components/ReadProgress.js
+++ src/components/ReadProgress.js
@@ -10,6 +10,7 @@ const ReadProgress = (props) => (
         <span className="progress-bar" />
       </span>
       <div className="progress-value">90%</div>
+      <div className="progress-value-completed">Completed </div>
     </div>
   </div>
 );
