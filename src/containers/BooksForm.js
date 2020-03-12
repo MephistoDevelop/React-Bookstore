88e729bb473c282dec16460b1577a9d56ccb06dd
MephistoDevelop <inesherald@gmail.com>
2020-03-12 05:49:01 -0600

    Add Post request to Rails API

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -49,7 +49,41 @@ class BooksForm extends React.Component {
   handleSubmit() {
     const { title, category, author } = this.state;
     const { createBook } = this.props;
+
     if (title !== '' && category !== '' && author !== '') {
+      const url = 'http://localhost:3000/books';
+      // const formData = new FormData();
+
+      // formData.append('title', title);
+      // formData.append('category', category);
+      // formData.append('author', author);
+      // formData.append('read_percent', '160');
+      const json = {
+        title, category, author, read_percent: '300',
+      };
+
+      // formData.append();
+      const xhr = new XMLHttpRequest();
+
+      // open the request with the verb and the url
+      xhr.open('POST', url);
+      xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+      // send the request
+      xhr.send(JSON.stringify(json));
+
+      console.log(xhr.responseText);
+      // const xhr = new XMLHttpRequest();
+      // let httpResult = [];
+
+      // xhr.addEventListener('load', () => {
+      //   // update the state of the component with the result here
+      //   httpResult = xhr.response;
+      //   console.log(`Im HTTP Request: ${httpResult}`);
+      // });
+      // // open the request with the verb and the url
+      // xhr.open('GET', 'http://127.0.0.1:3000/books');
+      // // send the request
+      // xhr.send();
       const newBook = {
         id: newID(),
         title,
